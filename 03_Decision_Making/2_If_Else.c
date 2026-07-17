2c65c0990ec56b4cea4c145f48cd01101841e316
sdxsaimon-creator <sdxsaimon@gmail.com>
2026-07-17 21:57:02 +0600

    Create 2_If_Else.c

---
+++ 03_Decision_Making/2_If_Else.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+    {
+        printf("Even Number");
+    }
+    else
+    {
+        printf("Odd Number");
+    }
+
+    return 0;
+}
