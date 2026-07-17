5b0826eeab13eadac8d3f435b78c1a22f46ee89e
sdxsaimon-creator <sdxsaimon@gmail.com>
2026-07-17 21:41:59 +0600

    Create 7_Positive_Negative.c

---
+++ 01_Basic/7_Positive_Negative.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num >= 0)
+    {
+        printf("Positive");
+    }
+    else
+    {
+        printf("Negative");
+    }
+
+    return 0;
+}
